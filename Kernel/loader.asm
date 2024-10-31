b3877f76041f59684fab4ef979318d31b07c9225
mothatceguy <mothatceguy@itba.edu.ar>
2024-10-31 17:53:14 -0300

    Refactor loader initialization and update help message in shell

--- Kernel/loader.asm
+++ Kernel/loader.asm
@@ -7,14 +7,15 @@ extern getStackBase
 loader:
 	call initializeKernelBinary	; Set up the kernel binary, and get thet stack address
 	mov rsp, rax				; Set up the stack with the returned address
+init:
+	call getStackBase
+	mov rsp, rax
 	call main
 hang:
 	cli
 	hlt	; halt machine should kernel return
 	jmp hang
 
-init:
-	call getStackBase
-	mov rsp, rax
-	call main
 	
+section .rodata
+userland equ 0x400000
