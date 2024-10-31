ca78c403467c1b59f27b65e13a934f65459c3c79
mothatceguy <mothatceguy@itba.edu.ar>
2024-10-31 16:42:15 -0300

    RUN SH ANDAA

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 sudo chmod a+x ./Image/*
-# qemu-system-x86_64: -audiodev pa,id=speaker: Parameter 'driver' does not accept value 'pa'
-# sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 
 
 ## MACOS
@@ -12,5 +10,5 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 elif grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null; then
     sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker,server=/mnt/wslg/PulseServer -machine pcspk-audiodev=speaker
 else
-    echo "Running on Linux"
+    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 fi
