c997980f513c1525e88d4a069c39f2c16ed21531
mcortest <mcortesteyssier@itba.edu.ar>
2025-05-04 15:21:46 -0300

    Build: testing

---
+++ Userland/SampleCodeModule/tests/test_util.h
@@ -0,0 +1,11 @@
+#include <stdint.h>
+
+//src : https://github.com/alejoaquili/ITBA-72.11-SO/tree/main/kernel-development/tests
+
+uint32_t GetUint();
+uint32_t GetUniform(uint32_t max);
+uint8_t memcheck(void *start, uint8_t value, uint32_t size);
+int64_t satoi(char *str);
+void bussy_wait(uint64_t n);
+void endless_loop();
+void endless_loop_print(uint64_t wait);
