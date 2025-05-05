b3f782e3bfb82fbba11276a7789079490e6846a2
mothatceguy <mothatceguy@itba.edu.ar>
2025-05-05 17:55:11 -0300

    add PCB structure and process states definition

---
+++ Kernel/include/pcb.h
@@ -0,0 +1,21 @@
+#ifndef PCB_H
+#define PCB_H
+
+#include <stdint.h>
+
+typedef enum {
+    READY,
+    RUNNING,
+    BLOCKED,
+} ProcessState;
+
+typedef struct PCB {
+    int pid; 
+    int priority; 
+    ProcessState state; 
+    uint64_t rip; 
+    uint64_t rsp; 
+    uint64_t registers[8]; // GPRs
+} PCB;
+
+#endif // PCB_H
