13a7e57ad12ecb85d950313f2cb5852112e0b956
mothatceguy <mothatceguy@itba.edu.ar>
2025-05-31 20:17:09 -0300

    -m 512 como antes

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ fi
 
 ## MACOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 1024 -audiodev coreaudio,id=speaker -machine pcspk-audiodev=speaker $DEBUG
+    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev coreaudio,id=speaker -machine pcspk-audiodev=speaker $DEBUG
 ## WSL
 elif grep -qEi "(Microsoft|WSL)" /proc/version &> /dev/null
 then
-    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 1024 -audiodev pa,id=speaker,server=/mnt/wslg/PulseServer -machine pcspk-audiodev=speaker $DEBUG
+    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker,server=/mnt/wslg/PulseServer -machine pcspk-audiodev=speaker $DEBUG
 else
-    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 1024 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker $DEBUG
+    sudo qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker $DEBUG
 fi
