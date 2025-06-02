5a425b5e52ef54dce0020f0831c34bcd5688a477
mothatceguy <mothatceguy@itba.edu.ar>
2025-06-02 19:27:54 -0300

    remove outdated comments from clock.h

--- Kernel/include/clock.h
+++ Kernel/include/clock.h
@@ -12,19 +12,10 @@ typedef struct time{
     int year;
 } time_t;
 
-/**
- * RTC time
- * @param timeZone to adjust to the correct time zone. Must put the value as it is. May cause errors if
- * timeZone is invalid.
- */
 time_t getTime(int64_t timeZone);
 
 uint64_t getTimeParam(uint64_t param); // -1 si falla
 
-/**
- * Returns the difference in milliseconds between two time_t structs.
- * Assumes both times are in the same timezone.
- */
 int64_t diffTimeMillis(time_t start, time_t end);
 
 #endif // CLOCK_H
